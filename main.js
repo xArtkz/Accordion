b32eb7c5cf7501efa684dabc753f29d676c15943
xArtkz <49668596+xArtkz@users.noreply.github.com>
2021-06-18 17:49:46 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+const accordionItemHeader = document.querySelectorAll(".accordion-item-header")
+
+accordionItemHeader.forEach(accordionItemHeader => {
+    accordionItemHeader.addEventListener("click", event => {
+        const currentlyActiveAccordionItemHeader = document.querySelector(".accordion-item-header.active")
+        if(currentlyActiveAccordionItemHeader && currentlyActiveAccordionItemHeader!== accordionItemHeader) {
+            currentlyActiveAccordionItemHeader.classList.toggle("active")
+            currentlyActiveAccordionItemHeader.nextElementSibling.style.maxHeight = 0
+        }
+
+        accordionItemHeader.classList.toggle("active")
+        const accordionItemBody = accordionItemHeader.nextElementSibling
+        if(accordionItemHeader.classList.contains("active")) {
+            accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px"
+        } else {
+            accordionItemBody.style.maxHeight = 0
+        }
+    })
+})
